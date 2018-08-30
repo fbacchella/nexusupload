3c390ce9c393574af600fafab3e4f4cb180f2322
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2018-08-30 12:04:23 +0200

    Updating setup.py

--- setup.py
+++ setup.py
@@ -42,8 +42,6 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
     ],
-    platforms=["Posix", "MacOS X"],
-    test_suite='tests',
+    platforms=["Posix"],
 )
