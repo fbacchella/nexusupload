1ab6d1b9ac02a6d219da250bac0dc6ea661bd6a6
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2023-06-20 09:59:48 +0200

    Updated makevent

--- makevenv.sh
+++ makevenv.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
+MINOR=${MINOR:-7}
+VENV=$PWD/venv3${MINOR}.${HWNAME}
+PYTHONCMD=$(command -v python3.$MINOR)
 
-VENV=$PWD/venv34
-virtualenv-3.4 $VENV
-PATH=/opt/local/bin:$PATH PYCURL_SSL_LIBRARY=openssl $VENV/bin/python setup.py install
+virtualenv-3 -p $PYTHONCMD $VENV
+PYCURL_CURL_CONFIG=$(type -p curl-config) PYCURL_SSL_LIBRARY=${PYCURL_SSL_LIBRARY:-openssl} $VENV/bin/python setup.py install
